b82db2e9de544712b0fe3f1ddd29455b54ea732b
ealexandergarcia <egarciapame@gmail.com>
2024-02-23 16:42:33 -0500

    Solucion ejercicio 6

--- algoritmoParaConvertirGradosCelsiusAFahrenheit.py
+++
@@ -1,5 +0,0 @@
-#Algoritmo para convertir grados celsius a fahrenheit
-valC = int(input("Ingrese el valode de los grados en Celsius que desea convertir: "))
-print("Valor Celsius: ",valC,"°C")
-ValF = ((9/5) * valC) + 32
-print("Valor Fahrenheit ",round(ValF, 1),"°F")
---
+++ algoritmoParaDeterminarSiUnNumeroEsParOImpar.py
@@ -0,0 +1,6 @@
+#Algoritmo para determinar si un numero es par o impar
+num1 = int(input("Ingrese el numero para saber si es par o impar: "))
+if num1 % 2 == 0:
+    print("El numero ",num1 ," es par")
+else:
+    print("El numero ", num1," es impar")
