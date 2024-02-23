af7444422833b8d1742149adc3e39db09468601f
ealexandergarcia <egarciapame@gmail.com>
2024-02-23 16:59:57 -0500

    Solucion ejercicio 7

---
+++ algoritmoParaCalcularElAreaDeUnTriangulo.py
@@ -0,0 +1,8 @@
+#Algoritmo para calcular el area de un triangulo
+base = int(input("Ingrese el valor de la base: "))
+altura = int(input("Ingrese el valor de la altura: "))
+
+area = int((base * altura)/2)
+
+print("Base: ",base," | Altura: ",altura)
+print("Area del tringulo: ",area)
